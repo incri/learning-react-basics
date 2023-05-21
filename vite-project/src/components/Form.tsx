0d6d1bbc8cc9f879d9a07d6cbd6b6aa75a7c84ba
incri <paudelbaibhav0@gmail.com>
2023-05-22 01:48:10 +0545

    added validation to form using formState to display error to user

--- vite-project/src/components/Form.tsx
+++ vite-project/src/components/Form.tsx
@@ -2,16 +2,14 @@ import React, { useState } from 'react'
 import { FormEvent } from 'react'
 import { FieldValue, FieldValues, useForm } from 'react-hook-form'
 
-
+interface FormData{
+    name: string;
+    age:number;
+}
 const Form = () => {
 
-    const {register, handleSubmit} = useForm()
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>()
 
-
-    const [person, setPerson] = useState ({
-        name: "",
-        age : ""
-    });
 
     const onSubmit = (data:FieldValues) => console.log(data);
     
@@ -20,12 +18,16 @@ const Form = () => {
         <h2 className="mb-3 mt-4"><b>Form</b></h2>
     <form onSubmit={handleSubmit(onSubmit)}><div className="mb-3">
         <label htmlFor="name" className="form-label">Name</label>
-        <input {...register('name')} id='name' type="text" className="form-control" />
+        <input {...register('name', {required:true, minLength:3})} id='name' type="text" className="form-control" />
+        {errors.name?.type === 'required' && <p className='text-danger'>The name field is required</p>}
+        {errors.name?.type === 'minLength' && <p className='text-danger'>The name must be at least 3 character</p>}
+
     </div>
 
     <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
         <input  {...register('age')} id='age' type="number" className="form-control" />
+
     </div>
     <button className='btn btn-primary'>Submit</button>
     </form>
