7d37c929c4b568179bc809c6fb5d2aa50b38485e
incri <paudelbaibhav0@gmail.com>
2023-05-22 01:01:38 +0545

    Form submit using useform hook

--- vite-project/src/components/Form.tsx
+++ vite-project/src/components/Form.tsx
@@ -1,28 +1,31 @@
 import React, { useState } from 'react'
 import { FormEvent } from 'react'
+import { FieldValue, FieldValues, useForm } from 'react-hook-form'
+
 
 const Form = () => {
+
+    const {register, handleSubmit} = useForm()
+
 
     const [person, setPerson] = useState ({
         name: "",
         age : ""
     });
+
+    const onSubmit = (data:FieldValues) => console.log(data);
     
-    const handleSubmit = (event: FormEvent) => {
-        event.preventDefault();
-        console.log(person)
-    }
   return (
     <>
         <h2 className="mb-3 mt-4"><b>Form</b></h2>
-    <form onSubmit={handleSubmit}><div className="mb-3">
+    <form onSubmit={handleSubmit(onSubmit)}><div className="mb-3">
         <label htmlFor="name" className="form-label">Name</label>
-        <input value={person.name} onChange={(event) => setPerson({...person,name:event.target.value})}id='name' type="text" className="form-control" />
+        <input {...register('name')} id='name' type="text" className="form-control" />
     </div>
 
     <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
-        <input value={person.age} onChange={(event) => setPerson({...person, age: event.target.value})} id='age' type="number" className="form-control" />
+        <input  {...register('age')} id='age' type="number" className="form-control" />
     </div>
     <button className='btn btn-primary'>Submit</button>
     </form>
